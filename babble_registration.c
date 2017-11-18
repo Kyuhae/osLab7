7a295e9586fa180cffa18ad24c425e7c972d2f84
Kyuhae <phisc255@student.liu.se>
2017-11-18 22:54:30 +0100

    stage1 / 3 working: multiple communicator and executor threads. Follow and stress tests pass.

---
+++ babble_registration.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <strings.h>
+#include <pthread.h>
+
+#include "babble_registration.h"
+
+client_bundle_t *registration_table[MAX_CLIENT];
+int nb_registered_clients;
+pthread_rwlock_t regTableLock;
+
+void registration_init(void)
+{
+	pthread_rwlock_init(&regTableLock, NULL);
+    nb_registered_clients=0;
+
+    bzero(registration_table, MAX_CLIENT * sizeof(client_bundle_t*));
+}
+
+client_bundle_t* registration_lookup(unsigned long key)
+{
+	pthread_rwlock_rdlock(&regTableLock);
+    int i=0;
+    
+    for(i=0; i< nb_registered_clients; i++){
+        if(registration_table[i]->key == key){
+			client_bundle_t *tmp = registration_table[i];
+			pthread_rwlock_unlock(&regTableLock);
+            return tmp;
+        }
+    }
+	pthread_rwlock_unlock(&regTableLock);
+    return NULL;
+}
+
+int registration_insert(client_bundle_t* cl)
+{   
+	pthread_rwlock_rdlock(&regTableLock); 
+    if(nb_registered_clients == MAX_CLIENT){
+		pthread_rwlock_unlock(&regTableLock);
+        return -1;
+    }
+    
+    /* lookup to find if key already exists*/
+    pthread_rwlock_unlock(&regTableLock);
+    client_bundle_t* lp= registration_lookup(cl->key);
+    if(lp != NULL){
+        fprintf(stderr, "Error -- id % ld already in use\n", cl->key);
+        pthread_rwlock_unlock(&regTableLock);
+        return -1;
+    }
+
+    /* insert cl */
+    pthread_rwlock_wrlock(&regTableLock); 
+    registration_table[nb_registered_clients]=cl;
+    nb_registered_clients++;
+	pthread_rwlock_unlock(&regTableLock);
+    return 0;
+}
+
+
+client_bundle_t* registration_remove(unsigned long key)
+{
+    int i=0;
+    
+    pthread_rwlock_wrlock(&regTableLock); 
+    for(i=0; i<nb_registered_clients; i++){
+        if(registration_table[i]->key == key){
+            break;
+        }
+    }
+
+    if(i == nb_registered_clients){
+        fprintf(stderr, "Error -- no client found\n");
+        pthread_rwlock_unlock(&regTableLock);
+        return NULL;
+    }
+    
+    
+    client_bundle_t* cl= registration_table[i];
+
+    nb_registered_clients--;
+    registration_table[i] = registration_table[nb_registered_clients];
+	pthread_rwlock_unlock(&regTableLock);
+
+    return cl;
+}
